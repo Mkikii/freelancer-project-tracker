49b270fae88de7709706d8765e2417f6af579509
Mkikii <maureenwkarimi@gmail.com>
2025-08-27 10:39:13 +0300

    modified models.py

--- models.py
+++ models.py
@@ -1,7 +1,3 @@
-# models.py - Database Models for Freelancer Project & Time Tracker
-# Created by: Maureen W Karimi
-# Phase 3 Python CLI Project - Moringa School
-
 from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, ForeignKey, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
